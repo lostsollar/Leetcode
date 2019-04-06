8e2a965c590252e17a492a6324727e5eb731153f
lostsollar <lostsollar@gmail.com>
2019-04-06 20:07:21 +0800

    +) Add 1 Two Sum

---
+++ 1 Two Sum/solution.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        one-pass hash table
+
+        Parameters
+        ----------
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_dict = {}
+        for i, x in enumerate(nums):
+            if target - x in nums_dict:
+                return [nums_dict[target - x], i]
+            nums_dict[x] = i
+
+
+checker = Solution()
+nums = [2, 7, 11, 15]
+target = 9
+nums1 = [3, 2, 4]
+target1 = 6
+nums2 = [3, 3]
+target2 = 6
+nums3 = [230,863,916,585,981,404,316,785,88,12,70,435,384,778,887,755,740,337,86,92,325,422,815,650,920,125,277,336,221,847,168,23,677,61,400,136,874,363,394,199,863,997,794,587,124,321,212,957,764,173,314,422,927,783,930,282,306,506,44,926,691,568,68,730,933,737,531,180,414,751,28,546,60,371,493,370,527,387,43,541,13,457,328,227,652,365,430,803,59,858,538,427,583,368,375,173,809,896,370,789]
+target3 = 542
+print checker.twoSum(nums3, target3)
